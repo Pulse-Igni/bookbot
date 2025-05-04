b977824b3d078fae9ef8aa8145532905d3017e2e
Setho <retsudog@gmail.com>
2025-05-04 21:53:02 +1000

    updated to give exception message

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ from stats import char_counter
 from stats import report
 import sys
 
+def main(book):
+    try:   
+        book_locale = book
+        report(get_book_text(book),book_locale)
+    except FileNotFoundError:
+        print ("File Not Found. Usage: python3 main.py <path_to_book>")
 
 # Retrieve book from:
 def get_book_text(address):
     with open(address) as book:
         book_contents = book.read()
     return book_contents
-
-
-def main(book):   
-    book_locale = book
-    report(get_book_text(book),book_locale)
-    
-
 
 #sys arg
 if len(sys.argv) != 2:
